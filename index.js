0485caa89703d520675df6b0b5a53a159c22494e
Rafael Caixeta <caixetadev@gmail.com>
2021-12-23 20:11:27 -0300

    API rest crud

--- index.js
+++ index.js
@@ -1,6 +1,25 @@
 const express = require('express')
+const mongoose = require('mongoose')
+require('dotenv').config()
 const app = express()
+const personRoutes = require('./routes/personRoutes')
+
+const DB_USER = process.env.DB_USER
+const DB_PASSWORD = process.env.DB_PASSWORD
+
+app.use(
+  express.urlencoded({ extended: true })
+)
+
 
 app.get('/', (req, res) => res.json({ message: 'Hello World' }))
 
-app.listen(3000)
+app.use(express.json())
+app.use('/person', personRoutes)
+
+mongoose.connect(`mongodb+srv://${DB_USER}:${DB_PASSWORD}@apicluster.ihcfy.mongodb.net/bancodaapi?retryWrites=true&w=majority`)
+  .then(() => {
+    console.log("Conectamos ao MongoDB")
+    app.listen(3000)
+  })
+  .catch(e => console.log(e))
